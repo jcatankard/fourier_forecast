95f5da8a8690eadd46e389654ce80647edc70bba
e10110199 <joshua.tankard@vista.com>
2024-01-04 19:13:15 +0100

    add test for log_y

--- tests/test_fourier_forecast.py
+++ tests/test_fourier_forecast.py
@@ -7,31 +7,39 @@ import unittest
 class TestFourierForecast(unittest.TestCase):
 
     atol = 0.1
-    rtol = np.inf
+    rtol = 0.1
     n_tests = 10
+
+    @staticmethod
+    def get_fourier_terms():
+        return np.array([np.random.choice(3), np.random.choice(5), np.random.choice(5), np.random.choice(10)])
 
     def test_basic(self):
         for i in range(self.n_tests):
             print(f'basic tests: {i + 1}')
-            fourier_terms = np.random.choice(list(range(5)), 4)
+            fourier_terms = self.get_fourier_terms()
             _, y, _ = create_data(regressors=False, fourier_terms=fourier_terms)
             ff = FourierForecast(*fourier_terms)
             ff.fit(y)
-            np.testing.assert_allclose(y, ff.fitted(), atol=self.atol, rtol=self.rtol)
+
+            assert np.abs(ff.fitted() - y).mean() < self.atol
+            np.testing.assert_allclose(ff.fitted(), y, atol=self.atol, rtol=self.rtol)
 
     def test_regressors(self):
         for i in range(self.n_tests):
             print(f'regressors tests: {i + 1}')
-            fourier_terms = np.random.choice(list(range(3)), 4)
+            fourier_terms = self.get_fourier_terms()
             _, y, r = create_data(regressors=True, fourier_terms=fourier_terms)
             ff = FourierForecast(*fourier_terms)
             ff.fit(y, regressors=r)
-            np.testing.assert_allclose(y, ff.fitted(), atol=self.atol, rtol=self.rtol)
+
+            assert np.abs(ff.fitted() - y).mean() < self.atol
+            np.testing.assert_allclose(ff.fitted(), y, atol=self.atol, rtol=self.rtol)
 
     def test_sample_weight(self):
         for i in range(self.n_tests):
             print(f'sample weight test: {i + 1}')
-            fourier_terms = np.random.choice(list(range(2)), 4)
+            fourier_terms = self.get_fourier_terms()
             _, y, _ = create_data(regressors=False, fourier_terms=fourier_terms)
 
             # if apply zero weight to the first n values, then we can multiply them by a random number and it
@@ -42,4 +50,17 @@ class TestFourierForecast(unittest.TestCase):
 
             ff = FourierForecast(*fourier_terms)
             ff.fit(y, sample_weight=w)
-            np.testing.assert_allclose(y[n:], ff.fitted()[n:], atol=self.atol, rtol=self.rtol)
+
+            assert np.abs(ff.fitted() - y).mean() < self.atol
+            np.testing.assert_allclose(ff.fitted()[n:], y[n:], atol=self.atol, rtol=self.rtol)
+
+    def test_log_y(self):
+        for i in range(self.n_tests):
+            print(f'log y tests: {i + 1}')
+            fourier_terms = self.get_fourier_terms()
+            _, y, _ = create_data(regressors=False, fourier_terms=fourier_terms, log_y=True)
+            ff = FourierForecast(*fourier_terms, log_y=True)
+            ff.fit(y)
+
+            assert np.abs(ff.fitted() - y).mean() < self.atol
+            np.testing.assert_allclose(ff.fitted(), y, atol=self.atol, rtol=self.rtol)
