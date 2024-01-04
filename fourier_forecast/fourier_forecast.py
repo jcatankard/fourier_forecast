114741c26b681edf4ade5cde9f9798d2a9671e56
e10110199 <joshua.tankard@vista.com>
2024-01-04 11:48:51 +0100

    replace gradient_descent.py with closed-form solution

--- fourier_forecast/fourier_forecast.py
+++ fourier_forecast/fourier_forecast.py
@@ -1,6 +1,3 @@
-from fourier_forecast.predict import predict, create_trend, create_bias
-from fourier_forecast.gradient_descent import gradient_descent
-from fourier_forecast.waves import sin_wave
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 from typing import Optional
@@ -13,14 +10,9 @@ class FourierForecast:
                  weekly_seasonality_terms: int = 3,
                  monthly_seasonality_terms: int = 0,
                  quarterly_seasonality_terms: int = 0,
-                 yearly_seasonality_terms: int = 10,
-                 learning_rate: float = 0.001,
-                 n_iterations: int = 100_000,
-                 tol: float = 1e-05
+                 yearly_seasonality_terms: int = 10
                  ):
-        self.learning_rate: float = float(learning_rate)
-        self.n_iterations: int = int(n_iterations)
-        self.tol = float(tol)
+        # add params for linear/flat trend, fit-intercept, regularization terms
 
         seasonality_terms = {
             7.: weekly_seasonality_terms,
@@ -29,60 +21,76 @@ class FourierForecast:
             365.25: yearly_seasonality_terms
         }
         self.seasonality_terms = {k: v for k, v in seasonality_terms.items() if v > 0}
-        self.amplitudes: Optional[NDArray[np.float64]] = None
-        self.phases: Optional[NDArray[np.float64]] = None
-        self.frequencies: Optional[NDArray[np.float64]] = None
-        self.trend: Optional[float] = None
-        self.bias: Optional[float] = None
+        self.n_waves = 2 * sum(self.seasonality_terms.values())
 
         self.regressors: Optional[NDArray[np.float64]] = None
-        self.regressor_weights: Optional[NDArray[np.float64]] = None
         self.n_regressors: Optional[int] = None
 
         self.ds: Optional[NDArray[np.int64]] = None
         self.pred_start: Optional[int] = None
         self.y: Optional[NDArray[np.float64]] = None
         self.sample_weight: Optional[NDArray[np.float64]] = None
+        self.x_: Optional[NDArray[np.float64]] = None
+        self.params_: Optional[NDArray[np.float64]] = None
 
     @staticmethod
     def _to_numpy(a) -> NDArray[np.float64]:
         return np.asarray(a, dtype=np.float64, order='C')
 
-    def _initiate_seasonality_estimates(self):
-        n_waves = 2 * sum(self.seasonality_terms.values())
-        self.amplitudes = np.zeros(n_waves, dtype=np.float64)
-        self.phases = np.zeros(n_waves, dtype=np.float64)
-        self.frequencies = np.zeros(n_waves, dtype=np.float64)
-        y_detrend = self.y - (self.ds * self.trend + self.bias)
+    def fit(self,
+            y: NDArray[np.float64],
+            regressors: Optional[NDArray[np.float64]] = None,
+            sample_weight: Optional[NDArray[np.float64]] = None
+            ):
+        self.y = self._to_numpy(y)
+        self._initiate_sample_weight(sample_weight)
+
+        self.pred_start = y.size
+        self.ds = np.arange(0, y.size, dtype=np.int64)
+
+        self.x_ = np.concatenate([
+            self._initiate_bias(self.ds),
+            self._initiate_trend(self.ds),
+            self._initiate_seasonalities(self.ds),
+            self._initiate_regressors(regressors, y.size, 0)
+        ], axis=1)
+
+        penalty = self._initiate_regularization_penalty()
+        self.params_ = np.linalg.inv(self.x_.T @ self.x_ + penalty) @ self.x_.T @ self.y
+
+    def _initiate_regularization_penalty(self) -> NDArray[np.int64]:
+        penalty = np.identity(self.x_.shape[1])
+        penalty[0][0] = 0  # for intercept
+        penalty *= 0  # set to zero for now
+        return penalty
+
+    def _initiate_seasonalities(self, ds: NDArray[np.int64]) -> NDArray[np.float64]:
         count = 0
+        waves = np.empty((self.n_waves, ds.size), dtype=np.float64)
         for periods, terms in self.seasonality_terms.items():
             for j in range(terms):
                 f = (j + 1) / periods
-                self.frequencies[count] = f
-                self.frequencies[count + 1] = f
-                self.phases[count] = 0
-                self.phases[count + 1] = np.pi / 2
-
-                # windows = np.lib.stride_tricks.sliding_window_view(y_detrend, int(1 / f))
-                # amp_int = (windows.max(axis=1) - windows.min(axis=1)).mean() / 2
-                self.amplitudes[count] = 1  # amp_int / 2
-                self.amplitudes[count + 1] = 1  # amp_int / 2
+                waves[count] = np.sin(2 * np.pi * ds * f, dtype=np.float64)
+                waves[count + 1] = np.cos(2 * np.pi * ds * f, dtype=np.float64)
                 count += 2
 
-    def _initiate_trend_estimates(self):
-        x = np.concatenate((np.ones((self.y.size, 1), dtype=np.float64),
-                            np.arange(self.y.size, dtype=np.float64).reshape(-1, 1)
-                            ), axis=1
-                           )
-        self.bias, self.trend = np.linalg.inv(x.T @ x) @ x.T @ self.y
+        return waves.T
+
+    @staticmethod
+    def _initiate_bias(ds: NDArray[np.int64]) -> NDArray[np.float64]:
+        """create intercept. later allow option for no intercept."""
+        return np.ones(shape=(ds.size, 1), dtype=np.float64)
+
+    @staticmethod
+    def _initiate_trend(ds: NDArray[np.int64]) -> NDArray[np.float64]:
+        """create trend array. later allow option for flat trend."""
+        return ds.astype(np.float64).reshape(-1, 1)
 
     def _initiate_regressors(self, regressors: Optional[NDArray], size: int, default_width: int) -> NDArray[np.float64]:
         regressors = np.zeros((size, default_width), dtype=np.float64) \
             if regressors is None else self._to_numpy(regressors)
 
-        if self.n_regressors is None:
-            self.n_regressors = regressors.shape[1]
-            self.regressor_weights = np.zeros(self.n_regressors, dtype=np.float64)
+        self.n_regressors = regressors.shape[1]
 
         return regressors
 
@@ -99,60 +107,21 @@ class FourierForecast:
             sample_weight = self._to_numpy(sample_weight)
             self.sample_weight = sample_weight / sample_weight.max()
 
-    def fit(self,
-            y: NDArray[np.float64],
-            regressors: Optional[NDArray[np.float64]] = None,
-            sample_weight: Optional[NDArray[np.float64]] = None
-            ):
-        self.y = self._to_numpy(y)
-        self._initiate_sample_weight(sample_weight)
-
-        self.pred_start = y.size
-        self.ds = np.arange(0, y.size, dtype=np.int64)
-
-        self._initiate_trend_estimates()
-        self._initiate_seasonality_estimates()
-        self.regressors = self._initiate_regressors(regressors, y.size, 0)
-
-        results = gradient_descent(self.bias,
-                                   self.trend,
-                                   self.amplitudes,
-                                   self.phases,
-                                   self.frequencies,
-                                   self.regressors,
-                                   self.regressor_weights,
-                                   self.y,
-                                   self.learning_rate,
-                                   self.n_iterations,
-                                   self.tol,
-                                   self.sample_weight
-                                   )
-        self.bias, self.trend, self.amplitudes, self.phases, self.frequencies, self.regressor_weights = results
-
     def fitted(self) -> NDArray[np.float64]:
-        return predict(0,
-                       self.y.size,
-                       self.bias,
-                       self.trend,
-                       self.amplitudes,
-                       self.phases,
-                       self.frequencies,
-                       self.regressors,
-                       self.regressor_weights
-                       )
+        return self.x_ @ self.params_
 
     def predict(self, h: int = 1, regressors: Optional[NDArray[np.float64]] = None) -> NDArray[np.float64]:
-        regressors = self._initiate_regressors(regressors, h, self.n_regressors)
-        return predict(self.pred_start,
-                       h,
-                       self.bias,
-                       self.trend,
-                       self.amplitudes,
-                       self.phases,
-                       self.frequencies,
-                       regressors,
-                       self.regressor_weights
-                       )
+
+        ds = np.arange(self.pred_start, self.pred_start + h, dtype=np.int64)
+
+        x = np.concatenate([
+            self._initiate_bias(ds),
+            self._initiate_trend(ds),
+            self._initiate_seasonalities(ds),
+            self._initiate_regressors(regressors, h, self.n_regressors)
+        ], axis=1)
+
+        return x @ self.params_
 
     @staticmethod
     def subplot(ax, ds: NDArray[np.int64], data: NDArray[np.float64], label: str):
@@ -164,23 +133,27 @@ class FourierForecast:
         n_rows = 2 + np.ceil(n_seasonalities / 2).astype(np.int64)
         fig, ax = plt.subplots(nrows=n_rows, ncols=2)
 
-        self.subplot(ax[0, 0], self.ds, create_bias(self.bias, self.ds), 'bias')
-        self.subplot(ax[0, 1], self.ds, create_trend(self.trend, self.ds), 'trend')
-        self.subplot(ax[1, 0], self.ds, self.regressors @ self.regressor_weights, 'regressors')
+        self.subplot(ax[0, 0], self.ds, self.x_[:, 0] * self.params_[0], 'bias')
+        self.subplot(ax[0, 1], self.ds, self.x_[:, 1] * self.params_[1], 'trend')
+
+        regs = self.x_[:, -self.n_regressors:] @ self.params_[-self.n_regressors:]
+        self.subplot(ax[1, 0], self.ds, regs, 'regressors')
+
         self.subplot(ax[1, 1], self.ds, self.y - self.fitted(), 'noise')
 
-        n = 0
-        for i, (periods, terms) in enumerate(self.seasonality_terms.items()):
+        start_col = 2
+        for i, (periods, n_terms) in enumerate(self.seasonality_terms.items()):
             n_to_plot = np.ceil(periods).astype(np.int64)
             ds = np.arange(n_to_plot)
-            s = np.zeros_like(ds, np.float64)
-            for j in range(terms):
-                s += sin_wave(self.amplitudes[n], self.phases[n], self.frequencies[n], ds)
-                s += sin_wave(self.amplitudes[n + 1], self.phases[n + 1], self.frequencies[n + 1], ds)
-                n += 2
+
+            end_col = start_col + n_terms * 2
+            s = self.x_[:, start_col: end_col] @ self.params_[start_col: end_col]
+
             row = 2 + i // 2
             col = i % 2
-            self.subplot(ax[row, col], ds, s, f'seasonality: periods={periods}')
+            self.subplot(ax[row, col], ds, s[: n_to_plot], f'seasonality: periods={periods}')
+
+            start_col = end_col
 
         if n_seasonalities % 2 == 1:
             ax[n_rows - 1, 1].axis('off')
