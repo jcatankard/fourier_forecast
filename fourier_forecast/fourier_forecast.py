3238ac39e2383110348fadb6514709aa14a4588d
e10110199 <joshua.tankard@vista.com>
2024-01-05 12:00:35 +0100

    add doc strings

--- fourier_forecast/fourier_forecast.py
+++ fourier_forecast/fourier_forecast.py
@@ -13,6 +13,7 @@ DAYS_IN_YEAR = 365.25
 
 
 class FourierForecast:
+    """Timeseries forecaster for daily timeseries using Fourier series, trend and exogenous regressors"""
 
     def __init__(self,
                  weekly_seasonality_terms: int = 3,
@@ -24,6 +25,32 @@ class FourierForecast:
                  regressor_reg: float = 0.0,
                  log_y: bool = False
                  ):
+        """
+        :param weekly_seasonality_terms: int, default=3
+            - number of Fourier terms to generate for fitting seasonality of 7 days
+        :param monthly_seasonality_terms: int, default=0
+            - number of Fourier terms to generate for fitting seasonality of 30.43 days
+        :param quarterly_seasonality_terms: int, default=0
+            - number of Fourier terms to generate for fitting seasonality of 91.31 days
+        :param yearly_seasonality_terms: int, default=10
+            - number of Fourier terms to generate for fitting seasonality of 365.25 days
+        :param trend_reg: float, default=0
+            - parameter regulating strength of trend fit
+            - smaller values (~0-1) allow the model to fit larger seasonal fluctuations,
+            - larger values (~1-100) dampen the seasonality.
+        :param seasonality_reg: float, default=0
+            - parameter regulating strength of seasonality fit
+            - smaller values (~0-1) allow the model to fit larger seasonal fluctuations,
+            - larger values (~1-100) dampen the seasonality.
+        :param regressor_reg: float, default=0
+            - parameter regulating strength of regressors fit
+            - smaller values (~0-1) allow the model to fit larger seasonal fluctuations,
+            - larger values (~1-100) dampen the seasonality.
+        :param log_y: bool, default=True
+            - takes the natural logarithm of the timeseries before fitting (and the exponent after predicting)
+            - all values must be positive or reverts bact to False
+            - useful for fitting interactive effects between seasonality, trend and regressors
+        """
 
         seasonality_terms = {
             DAYS_IN_WEEK: weekly_seasonality_terms,
@@ -53,15 +80,17 @@ class FourierForecast:
         self.x_: Optional[NDArray[np.float64]] = None
         self.params_: Optional[NDArray[np.float64]] = None
 
-    @staticmethod
-    def _to_numpy(a) -> NDArray[np.float64]:
-        return np.asarray(a, dtype=np.float64, order='C')
-
     def fit(self,
             y: NDArray[np.float64],
             regressors: Optional[NDArray[np.float64]] = None,
             sample_weight: Optional[NDArray[np.float64]] = None
             ):
+        """
+        Sets parameter weights for bias, trend, seasonality and regressor terms
+        :param y: timeseries target at daily frequency
+        :param regressors: optional, regressors to fit corresponding to y
+        :param sample_weight: optional, individual weights for each sample - all values must be non-negative
+        """
 
         if (y.min() <= 0) and self.log_y:
             self.log_y = False
@@ -85,6 +114,37 @@ class FourierForecast:
 
         penalty = self._initiate_regularization_penalty()
         self.params_ = np.linalg.inv(self.x_.T @ self.x_ + penalty) @ self.x_.T @ self.y
+
+    def fitted(self) -> NDArray[np.float64]:
+        """returns fitted values"""
+        y_hat = self.x_ @ self.params_
+        return np.exp(y_hat) if self.log_y else y_hat
+
+    def predict(self, h: int = 1, regressors: Optional[NDArray[np.float64]] = None) -> NDArray[np.float64]:
+        """
+        :param h: number of horizons to forecast
+        :param regressors: optional regressors if known in the future. If passing, must align to regressors seen at fit.
+        """
+        ds = np.arange(self.pred_start, self.pred_start + h, dtype=np.int64)
+
+        x = np.concatenate([
+            self._initiate_bias(ds),
+            self._initiate_trend(ds),
+            self._initiate_seasonalities(ds),
+            self._initiate_regressors(regressors, h)
+        ], axis=1)
+
+        preds = x @ self.params_
+        return np.exp(preds) if self.log_y else preds
+
+    def plot_components(self) -> go.Figure:
+        return plot_components(self)
+
+    def plot_regressor_components(self, regressor_names: Optional[list[str]] = None) -> go.Figure:
+        return plot_regressor_components(self, regressor_names)
+
+    def plot_seasonality_components(self) -> go.Figure:
+        return plot_seasonality_components(self)
 
     def _rescale_data(self):
         """reference: https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/linear_model/_base.py"""
@@ -141,29 +201,6 @@ class FourierForecast:
             sample_weight = self._to_numpy(sample_weight)
             self.sample_weight = sample_weight / sample_weight.max()
 
-    def fitted(self) -> NDArray[np.float64]:
-        y_hat = self.x_ @ self.params_
-        return np.exp(y_hat) if self.log_y else y_hat
-
-    def predict(self, h: int = 1, regressors: Optional[NDArray[np.float64]] = None) -> NDArray[np.float64]:
-
-        ds = np.arange(self.pred_start, self.pred_start + h, dtype=np.int64)
-
-        x = np.concatenate([
-            self._initiate_bias(ds),
-            self._initiate_trend(ds),
-            self._initiate_seasonalities(ds),
-            self._initiate_regressors(regressors, h)
-        ], axis=1)
-
-        preds = x @ self.params_
-        return np.exp(preds) if self.log_y else preds
-
-    def plot_components(self) -> go.Figure:
-        return plot_components(self)
-
-    def plot_regressor_components(self, regressor_names: Optional[list[str]] = None) -> go.Figure:
-        return plot_regressor_components(self, regressor_names)
-
-    def plot_seasonality_components(self) -> go.Figure:
-        return plot_seasonality_components(self)
+    @staticmethod
+    def _to_numpy(a) -> NDArray[np.float64]:
+        return np.asarray(a, dtype=np.float64, order='C')
