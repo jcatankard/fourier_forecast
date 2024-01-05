08ba19832f062c178e187668f0b87482097eeed5
e10110199 <joshua.tankard@vista.com>
2024-01-05 15:53:58 +0100

    add files for pip deployment

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='fourier_forecast',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/jcatankard/fourier_forecast',
+    author='Josh Tankard',
+    description='Time-series forecasting with Fourier series',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'plotly'],
+)
