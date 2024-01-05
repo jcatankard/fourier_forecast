01fdd79c0860d0fef7fe53839f368cadc4c45f3c
e10110199 <joshua.tankard@vista.com>
2024-01-05 11:26:47 +0100

    remove duplication

--- examples/test_example.py
+++ examples/test_example.py
@@ -50,31 +50,3 @@ if __name__ == '__main__':
     fig.add_trace(go.Scatter(x=ds[n:], y=pred, mode='lines', name='preds'))
     fig.update_layout(showlegend=True)
     fig.show()
-
-    ff = FourierForecast(log_y=log_y,
-                         seasonality_reg=0,
-                         regressor_reg=0,
-                         trend_reg=0,
-                         **fourier_terms
-                         )
-    n = 700
-    start = time.time()
-    ff.fit(actuals[: n], regressors=regressors[: n])
-    print('Time to fit:', round(time.time() - start, 3))
-    ff.plot_components().show()
-    # ff.plot_seasonality_components().show()
-    # ff.plot_regressor_components().show()
-
-    pred = ff.predict(h=ds.size - n, regressors=regressors[n:])
-
-    print('PREDS VS ACTUALS:', np.abs(pred - actuals[n:]).mean().round(3))
-    print('FITTED vs ACTUALS:', np.abs(ff.fitted() - actuals[: n]).mean().round(3))
-    print('CLEAN vs ACTUALS:', np.abs(clean[: n] - actuals[: n]).mean().round(3))
-    print('PREDS vs CLEAN:', np.abs(pred - clean[n:]).mean().round(3))
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=ds, y=actuals, mode='lines', name='actuals'))
-    fig.add_trace(go.Scatter(x=ds[: n], y=ff.fitted(), mode='lines', name='fitted'))
-    fig.add_trace(go.Scatter(x=ds[n:], y=pred, mode='lines', name='preds'))
-    fig.update_layout(showlegend=True)
-    fig.show()
